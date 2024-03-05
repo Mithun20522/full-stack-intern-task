395a64faae6991baf47757d43c4811d616ecf28f
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-05 19:31:08 +0530

    read or get data api route done.

--- backend/controllers/data.controller.js
+++ backend/controllers/data.controller.js
@@ -1,10 +1,10 @@
 import Form from "../models/data.model.js";
 
-// create - data
+// create - data (C)
 export const createFormData = async (req, res) => {
     const { name, email, phonenumber, hobby } = req.body;
     if (!name || !email || !phonenumber || !hobby) {
-        res.status(400).send('All fields are mandatory');
+        return res.status(400).send('All fields are mandatory');
     } else {
         try {
             const newData = new Form({
@@ -14,9 +14,25 @@ export const createFormData = async (req, res) => {
                 hobby: hobby
             });
             await newData.save();
-            res.status(201).send(newData);
+            return res.status(201).send(newData);
         } catch (error) {
-            res.status(500).send('Internal server error');
+            return res.status(500).send('Internal server error');
         }
     }
+}
+
+// read data (R)
+export const getFormData = async(req, res) => {
+    try {
+        const fetchedData = await Form.find();
+        if(fetchedData.length === 0){
+            return res.status(404).send('No data found');
+        }
+        else{
+            return res.status(201).send(fetchedData);
+        }
+    } catch (error) {
+        
+    }
+
 }
--- backend/routes/data.route.js
+++ backend/routes/data.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { createFormData } from '../controllers/data.controller.js';
+import { createFormData, getFormData } from '../controllers/data.controller.js';
 
 const router = express.Router();
 
 // create data api route
 router.post('/create-data',createFormData);
+// read data api route
+router.get('/get-data', getFormData);
 
 export default router;
