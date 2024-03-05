4486caf48e7938d50018306e08f52239c61e4503
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-05 20:18:52 +0530

    layout for form and table finalized

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,11 @@
+import FormPopUp from "@/components/FormPopUp";
+import Table from "@/components/Table";
+
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="flex flex-col items-center mt-7">
+      <FormPopUp/>
+      <Table/>
     </div>
   );
 }
---
+++ frontend/src/components/FormPopUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormPopUp = () => {
+  return (
+    <div>FormPopUp</div>
+  )
+}
+
+export default FormPopUp
