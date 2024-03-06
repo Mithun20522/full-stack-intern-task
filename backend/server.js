77bc64c4f30f148e9be9fe4deea7270beafdf67f
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-06 13:56:30 +0530

    backend minor changes for deployment.

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,8 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import router from './routes/data.route.js';
 import cors from 'cors';
-import path from 'path';
 dotenv.config();
 
-const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -16,11 +14,6 @@ await mongoose.connect(process.env.MONGO_DB_URL)
     .catch(() => console.log('Error occurred while connecting mongoDB'));
 
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-app.get('*',(req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
     
 app.use('/api',router);
 
