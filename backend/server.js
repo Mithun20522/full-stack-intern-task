7dc9b2235ca746280b057599b0bfa508210f9241
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-06 13:43:17 +0530

    predploy config

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import router from './routes/data.route.js';
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -13,6 +16,12 @@ await mongoose.connect(process.env.MONGO_DB_URL)
     .catch(() => console.log('Error occurred while connecting mongoDB'));
 
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*',(req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+    
 app.use('/api',router);
 
 app.listen(process.env.PORT, () => console.log(`server is listening on PORT: ${process.env.PORT}`));
